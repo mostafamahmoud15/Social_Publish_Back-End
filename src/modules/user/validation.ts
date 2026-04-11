f16ccc89087e83c1ff5ff98e4c6337d511694248
mostafamahmoud15 <mostafa.mahmouud15@gmail.com>
2026-04-12 03:18:23 +0400

    fix

--- src/modules/user/validation.ts
+++ src/modules/user/validation.ts
@@ -3,10 +3,10 @@ import { z } from "zod";
 
 // Define the user schema
 export const createUserSchema = z.object({
-    username: z.string().min(3),
-    email: z.email(),
-    role: z.enum(["owner", "user"]),
-    password: z.string().regex(/^[!@#$%^&*]{1}[A-Za-z]{3,}[0-9]{2,}[!@#$%^&*]{1}$/),
+  username: z.string().min(3),
+  email: z.email(),
+  role: z.enum(["owner", "user"]),
+  password: z.string().min(6),
 });
 
 
